6506363045f3c9d6d7a882f6f9a932334b48aeb0
iti98 <etienne98.ch@gmail.com>
2022-10-04 14:39:00 +0200

    Adding architecture and rooting to project

---
+++ src/app/components/se-capsules/se-capsules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-se-capsules',
+  templateUrl: './se-capsules.component.html',
+  styleUrls: ['./se-capsules.component.css']
+})
+export class SeCapsulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
