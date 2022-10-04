0098a0d5cd0237289ab36140e3e91861649bfad2
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-10-04 16:00:47 +0200

    l'écran des capsules est fini et marche à la perfection (#3)
    * appel api
    
    * j'ai fini

--- src/app/services/space-xapi.service.ts
+++ src/app/services/space-xapi.service.ts
@@ -1,9 +1,18 @@
+import { Capsule } from './../models/capsule';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpaceXApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllCapsule(): Observable<Capsule[]> {
+    return this.http.get<Capsule[]>("https://api.spacexdata.com/v3/capsules");
+  }
 }
