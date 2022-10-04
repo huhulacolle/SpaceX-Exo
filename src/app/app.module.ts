bd8e3825c88fe19de9c12be512c85dbb429e904f
sacha0914 <96061674+sacha0914@users.noreply.github.com>
2022-10-04 16:32:46 +0200

    Feature/navbar (#4)
    * navbar
    
    * nav bar finito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeeLaunchesComponent } from './components/see-launches/see-launches.component';
 import { SeCapsulesComponent } from './components/se-capsules/se-capsules.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SeeLaunchesComponent,
-    SeCapsulesComponent
+    SeCapsulesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
