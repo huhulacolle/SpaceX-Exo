50f118150df39e8fb80474d51d8a0b7a604c3ad7
iti98 <45237055+iti98@users.noreply.github.com>
2022-10-05 09:20:20 +0200

    Fin de la partie tableau Launches (#5)

--- src/app/services/space-xapi.service.ts
+++ src/app/services/space-xapi.service.ts
@@ -2,17 +2,24 @@ import { Capsule } from './../models/capsule';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Launch } from "../models/launch";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpaceXApiService {
 
+  private _apiUrl = 'https://api.spacexdata.com/v3';
+
   constructor(
     private http: HttpClient
   ) { }
 
   getAllCapsule(): Observable<Capsule[]> {
-    return this.http.get<Capsule[]>("https://api.spacexdata.com/v3/capsules");
+    return this.http.get<Capsule[]>(`${this._apiUrl}/capsules`);
+  }
+
+  public getAllLaunches(): Observable<Launch[]> {
+    return this.http.get<Launch[]>(`${this._apiUrl}/launches`);
   }
 }
