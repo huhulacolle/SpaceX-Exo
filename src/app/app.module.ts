362dc43f9bf1fe620a85f6e1dcfbf74ecde61392
sacha0914 <96061674+sacha0914@users.noreply.github.com>
2022-10-05 16:19:21 +0200

    detail (#8)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { SeeLaunchesComponent } from './components/see-launches/see-launches.component';
 import { SeCapsulesComponent } from './components/se-capsules/se-capsules.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DetailCapsComponent } from './components/detail-caps/detail-caps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SeeLaunchesComponent,
     SeCapsulesComponent,
-    NavbarComponent
+    NavbarComponent,
+    DetailCapsComponent
   ],
   imports: [
     BrowserModule,
